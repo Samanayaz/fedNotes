503ad2f23d9b435f6a1b4fec039f7f4e4b711eaf
Saman Ayaz Ebrahim <samanmamdani@gmail.com>
2019-12-01 16:36:51 -0600

    numbers and math

---
+++ 06js_sandbox/05numbers_mathobject.js
@@ -0,0 +1,65 @@
+const num1 = 100; 
+const num = 50; 
+
+let value;
+
+// simple math with number 
+value = num1 + num2 
+value = num1 - num2
+value = num1 x num2 
+value = num1 / num2
+value = num1 % num2 
+//  modulus operator - finding a remainder 
+
+// Math object 
+//  'Math', here in this case, is an object, meaning that it has properties and methods 
+// - a property is basically like an attribute 
+//  - method is a function that is inside an object 
+
+value = Math.PI;
+value = Math.E;
+
+// methods 
+value = Math.round(2.4);
+value = Math.round(2.8);
+value = Math.ceil(2.3);
+// this to round up (cieling)
+value = Math.floor(2.9)
+// this is round down (floor)
+value = Math.sqrt(64);
+value = Math.abs(-3);
+valye = Math.pow(8,2);
+// 8 to the 2nd power
+
+value = Math.min(3,5,66,11,8,-8);
+// figuring out the minimum value 
+
+value = Math.max(5,3,55,-7)
+// figuring out highest value 
+
+value = Math.random()
+// generates a random number between 0 and 1 
+
+value = Math.random() * 20; 
+// generates a random number under 20
+
+value = Math.random() * 20 + 1 
+// genrrate a random number under 21 
+
+value = Math.floor(Mathrandom() *20 +1)
+// generates a random number btw 0 and 21 rounded down 
+
+value = (Math.random() * 20 + 1.toFixed(2); 
+// generates a random number btw 0 and 21 in two decimal places 
+
+
+
+
+value = 
+
+
+
+
+
+// output
+console.log(value);
